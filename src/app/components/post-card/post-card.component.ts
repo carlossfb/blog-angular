b4753937b40c00dd3bac4eb9670fa03d246bc8f6
Carlos Eduardo <72090014+carlossfb@users.noreply.github.com>
2023-06-02 22:00:00 -0300

    adicionando dinamismo com propriedades

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
   styleUrls: ['./post-card.component.scss']
 })
-export class PostCardComponent {
+export class PostCardComponent implements OnInit {
+  @Input()
+  authorName:string=""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  modifiedAt:string = ""
+  @Input()
+  readTime:string = ""
+
+  constructor(){}
+
+  ngOnInit(): void {
+  }
 
 }
